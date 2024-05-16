0aa999abd0ed03170e4bd44b98b5c8e73ec5fef3
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-05-16 18:57:15 +0530

    firebase config setup

--- src/components/authform/Login.jsx
+++ src/components/authform/Login.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div className='flex flex-col gap-5'>
-      <input className='w-[360px] h-[45px] border-2 rounded-md' type='email' />
-      <input
-        className='w-[360px] h-[45px] border-2 rounded-md'
-        type='password'
-      />
-      <div className='flex justify-center items-center mt-[12px]'>
+    <div className='flex flex-col gap-1'>
+      <div className='flex flex-col gap-1'>
+        <label className='text-slate-400 font-vollkorn' for='email'>
+          Email
+        </label>
+        <input
+          className='w-[360px] h-[45px] border-2 rounded-md'
+          type='email'
+        />
+      </div>
+      <div className='flex flex-col gap-1'>
+        <label className='text-slate-400 font-vollkorn' for='email'>
+          Password
+        </label>
+        <input
+          className='w-[360px] h-[45px] border-2 rounded-md'
+          type='password'
+        />
+      </div>
+
+      <div className='flex justify-center items-center mt-[29px]'>
         <button className='w-[360px] h-[37px] flex justify-center items-center text-slate-400 rounded bg-cyan-900 font-abril'>
           Log in
         </button>
--- src/components/authform/SignUp.jsx
+++ src/components/authform/SignUp.jsx
@@ -2,14 +2,33 @@ import React from 'react';
 
 const SignUp = () => {
   return (
-    <div className='flex flex-col gap-5'>
-      <input className='w-[360px] h-[45px] border-2 rounded-md' type='name' />
-      <input className='w-[360px] h-[45px] border-2 rounded-md' type='email' />
-      <input
-        className='w-[360px] h-[45px] border-2 rounded-md'
-        type='password'
-      />
-      <div className='flex justify-center items-center mt-[12px]'>
+    <div className='flex flex-col gap-1'>
+      <div className='flex flex-col gap-1'>
+        <label className='text-slate-400 font-vollkorn' for='name'>
+          Full name
+        </label>
+        <input className='w-[360px] h-[45px] border-2 rounded-md' type='name' />
+      </div>
+      <div className='flex flex-col gap-1'>
+        {' '}
+        <label className='text-slate-400 font-vollkorn' for='email'>
+          Email
+        </label>
+        <input
+          className='w-[360px] h-[45px] border-2 rounded-md'
+          type='email'
+        />
+      </div>
+      <div className='flex flex-col gap-1'>
+        <label className='text-slate-400 font-vollkorn' for='password'>
+          Password
+        </label>
+        <input
+          className='w-[360px] h-[45px] border-2 rounded-md'
+          type='password'
+        />
+      </div>
+      <div className='flex justify-center items-center mt-[29px]'>
         {' '}
         <button className='w-[360px] h-[37px] flex justify-center items-center text-slate-400 rounded bg-cyan-900 font-abril'>
           Sign up
